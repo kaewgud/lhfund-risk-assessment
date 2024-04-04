0fcf7b18b6cb2d48a721329ad155ea1b5b5d0f0e
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2024-04-04 18:45:58 +0700

    📚 Update portfolio router

--- src/server/api/routers/portfolio.ts
+++ src/server/api/routers/portfolio.ts
@@ -1,4 +1,3 @@
-
 import { z } from "zod";
 
 import {
@@ -18,4 +17,42 @@ export const portfolioRouter = createTRPCRouter({
     return ratios;
   }),
 
+  createRatio: adminOnlyProcedure
+    .input(z.object({
+      riskLevel: z.number(),
+      fundId: z.string(),
+      percentage: z.number(),
+    }))
+    .mutation(async ({ ctx, input }) => {
+
+      const riskLevel = await ctx.db.riskLevel.findFirst({
+        where: {
+          level: input.riskLevel,
+        },
+      });
+
+      if (!riskLevel) {
+        throw new Error("Risk Level not found");
+      }
+
+      const fund = await ctx.db.fund.findUnique({
+        where: {
+          id: input.fundId,
+        },
+      });
+
+      if (!fund) {
+        throw new Error("Fund not found");
+      }
+
+      return ctx.db.ratio.create({
+        data: {
+          name: fund.name as string,
+          riskLevelId: riskLevel.id as string,
+          fundId: input.fundId as string,
+          percentage: input.percentage as number,
+        },
+      });
+    }),
 })
+
