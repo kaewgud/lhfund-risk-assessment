6b20f923c5c06e01fa27761c6879df3bd8b48ec6
maycozygirl <may11682.o@gmail.com>
2024-04-05 22:45:49 +0700

    add: show chart

--- src/components/Showchart.tsx
+++ src/components/Showchart.tsx
@@ -8,12 +8,9 @@ export default function Showchart(){
             <CardBody>
             <p className="uppercase text-xs text-primary-300 mb-1"> PRIMARY TEXT </p>
             <p className="text-xl text-primary-100 font-bold">5.6787,34</p>
-            <p className="lowercasr text-xs text-primary-300 mb-5"> Secondary text </p>
+            <p className="lowercasr text-xs text-primary-300 mb-3"> Secondary text </p>
             <Divider/>
             <div className="mt-5"> <Piechart/> </div>
-            
-            
-            
             </CardBody>
         </Card>
     )
