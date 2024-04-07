8f1fec8d25cd90ea2b0f8b64a1d2ddac1dbfa32b
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2024-04-07 10:49:12 +0700

    Update admin page

--- src/components/Piechart.tsx
+++ src/components/Piechart.tsx
@@ -7,13 +7,13 @@ export default function Piechart() {
       series={[
         {
           data: [
-            { id: 0, value: 10, label: 'series A' },
-            { id: 1, value: 15, label: 'series B' },
-            { id: 2, value: 20, label: 'series C' },
+            { id: 0, value: 10, label: 'LHTREASURY-A' },
+            { id: 1, value: 20, label: 'LHTREASURY-L' },
+            { id: 2, value: 70, label: 'None', color: '#FFFFFF' },
           ],
         },
       ]}
-      width={290}
+      width={450}
       height={150}
     />
   );
